f3d4b5c52ae14a30a331b304caf50a478d0f30d2
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-24 11:02:09 +0200

    Fix Application showing Homepage briefly even when user is logged in

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,23 @@
-import React, { Suspense, useEffect, LazyExoticComponent } from 'react';
+import React, {
+  Suspense,
+  useEffect,
+  LazyExoticComponent,
+  useState,
+} from 'react';
 import { useAppSelector, useAppDispatch } from '../app/hooks';
 import { RootState, AppDispatch } from '../app/store';
 import { auth, onAuthStateChanged } from '../app/firebase';
 import { login, logout } from '../features/user/userSlice';
 import Loading from './Loading';
 
+const Blog: LazyExoticComponent<any> = React.lazy(() => import('./Blog'));
 const Homepage: LazyExoticComponent<any> = React.lazy(
   () => import('./Homepage')
 );
-const Blog: LazyExoticComponent<any> = React.lazy(() => import('./Blog'));
 
 const App: React.FC = () => {
   const user: RootState['user'] = useAppSelector((state) => state.user);
+  const [loading, setLoading] = useState(true);
   const dispatch: AppDispatch = useAppDispatch();
 
   useEffect(() => {
@@ -25,15 +31,19 @@ const App: React.FC = () => {
             photoUrl: userAuth.photoURL,
           })
         );
+        setLoading(false);
       } else {
         dispatch(logout());
+        setLoading(false);
       }
     });
   }, []);
 
   return (
     <div className="min-h-screen text-slate-800">
-      {user.data ? (
+      {loading ? (
+        <Loading />
+      ) : user.data ? (
         <Suspense fallback={<Loading />}>
           <Blog />
         </Suspense>
