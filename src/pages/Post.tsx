f9ae90793bcf5a0b5fa29ec680c664a44ba175c1
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-25 18:47:37 +0200

    Lazy load Post component, display loader until all components load

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Suspense } from 'react';
+import Loading from './Loading';
 import { useParams } from 'react-router-dom';
 import { getDoc, doc, collection, db } from '../app/firebase';
 import {
@@ -7,19 +8,38 @@ import {
   useCheckIfLoggedIn,
 } from '../app/hooks';
 import { serialize } from '../editor/serialize';
-import Header from '../components/Header';
-import Container from '../components/Container';
-import UserUI from '../components/UserUI';
-import LoginUI from '../components/LoginUI';
-import PostUI from '../components/PostUI';
-import Loading from './Loading';
-import Card from '../components/Card';
-import Alert from '../components/Alert';
-import Comments from '../components/Comments';
-import Author from '../components/Author';
 import type { DocumentData, CollectionReference } from 'firebase/firestore';
 import type { RootState } from '../app/store';
 import type { Node } from 'slate';
+import type { LazyExoticComponent } from 'react';
+
+const Header: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/Header')
+);
+const Container: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/Container')
+);
+const UserUI: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/UserUI')
+);
+const LoginUI: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/LoginUI')
+);
+const PostUI: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/PostUI')
+);
+const Card: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/Card')
+);
+const Alert: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/Alert')
+);
+const Comments: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/Comments')
+);
+const Author: LazyExoticComponent<any> = React.lazy(
+  () => import('../components/Author')
+);
 
 interface PostProps {}
 
@@ -45,48 +65,50 @@ const Post: React.FC<PostProps> = ({}) => {
   return post === null ? (
     <Loading />
   ) : (
-    <main>
-      <Header>{user.data ? <UserUI /> : <LoginUI />}</Header>
-      {post === undefined ? <Container>404! Post not found</Container> : ''}
-      {post ? (
-        <Container customClasses="my-14">
-          {alert.data.isShown ? (
-            <div className="my-4">
-              <Alert title={alert.data.title} variant={alert.data.variant}>
-                {alert.data.text}
-              </Alert>
+    <Suspense fallback={<Loading />}>
+      <main>
+        <Header>{user.data ? <UserUI /> : <LoginUI />}</Header>
+        {post === undefined ? <Container>404! Post not found</Container> : ''}
+        {post ? (
+          <Container customClasses="my-14">
+            {alert.data.isShown ? (
+              <div className="my-4">
+                <Alert title={alert.data.title} variant={alert.data.variant}>
+                  {alert.data.text}
+                </Alert>
+              </div>
+            ) : (
+              ''
+            )}
+            <div className="flex">
+              <PostUI
+                postID={params.id}
+                likes={post.likes}
+                bookmarks={post.bookmarks}
+              />
+              <div>
+                <Card customClasses="p-12">
+                  <Author
+                    avatarUrl={post.author.photoUrl}
+                    displayName={post.author.displayName}
+                    timestamp={post.timestamp}
+                  />
+                  <h1 className="my-8 text-5xl font-extrabold block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-500 to-slate-800">
+                    {post.title}
+                  </h1>
+                  {post.content.map((item: Node) => serialize(item))}
+                </Card>
+                <Card customClasses="p-12 mt-6">
+                  <Comments post={post} postID={params.id} />
+                </Card>
+              </div>
             </div>
-          ) : (
-            ''
-          )}
-          <div className="flex">
-            <PostUI
-              postID={params.id}
-              likes={post.likes}
-              bookmarks={post.bookmarks}
-            />
-            <div>
-              <Card customClasses="p-12">
-                <Author
-                  avatarUrl={post.author.photoUrl}
-                  displayName={post.author.displayName}
-                  timestamp={post.timestamp}
-                />
-                <h1 className="my-8 text-5xl font-extrabold block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-green-500 to-slate-800">
-                  {post.title}
-                </h1>
-                {post.content.map((item: Node) => serialize(item))}
-              </Card>
-              <Card customClasses="p-12 mt-6">
-                <Comments post={post} postID={params.id} />
-              </Card>
-            </div>
-          </div>
-        </Container>
-      ) : (
-        ''
-      )}
-    </main>
+          </Container>
+        ) : (
+          ''
+        )}
+      </main>
+    </Suspense>
   );
 };
 
