ad7e372292594e6b7323f3fdd5bcdc4a1feb4635
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-26 16:13:27 +0200

    Add menu custom classes to tailwind config for better code readability

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -16,23 +16,17 @@ const Menu: React.FC<MenuProps> = ({}) => {
     <nav className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
       <ul className="space-y-2">
         <li>
-          <Link
-            to="/"
-            className="text-xl flex items-center p-2 font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-          >
+          <Link to="/" className="nav-link">
             <Icon>
-              <Home className="text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+              <Home className="nav-icon" />
             </Icon>
             <span className="ml-3 text-base">Home</span>
           </Link>
         </li>
         <li>
-          <Link
-            to="/bookmarks"
-            className="text-xl flex items-center p-2 font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-          >
+          <Link to="/bookmarks" className="nav-link">
             <Icon>
-              <Bookmark className="text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+              <Bookmark className="nav-icon" />
             </Icon>
             <span className="ml-3 text-base">Bookmarks</span>
           </Link>
