5d45fd6d3fe7fcb537ce086af07893a55cdaf844
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-26 18:06:17 +0200

    Create Badge component

---
+++ src/components/Badge.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface BadgeProps {
+  children: React.ReactNode;
+}
+
+const Badge: React.FC<BadgeProps> = ({ children }) => {
+  return (
+    <span className="bg-gray-100 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 dark:bg-gray-700 dark:text-gray-300">
+      {children}
+    </span>
+  );
+};
+
+export default Badge;
