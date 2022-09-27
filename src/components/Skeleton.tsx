d8f6faef7b9cd04aa4ed28ba78380ca2bcb15b70
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-27 19:58:54 +0200

    Add small version of a Skeleton component

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -1,46 +1,60 @@
 import React from 'react';
 import Card from './Card';
 
-interface SkeletonProps {}
+interface SkeletonProps {
+  sm?: boolean;
+}
 
-const Skeleton: React.FC<SkeletonProps> = ({}) => {
-  return (
-    <Card customClasses="p-4 m-4">
-      <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
-        <div className="flex items-center space-x-2 w-full">
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-        </div>
-        <div className="flex items-center w-full space-x-2 max-w-[480px]">
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-        </div>
-        <div className="flex items-center w-full space-x-2 max-w-[400px]">
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-        </div>
-        <div className="flex items-center w-full space-x-2 max-w-[480px]">
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-        </div>
-        <div className="flex items-center w-full space-x-2 max-w-[440px]">
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
-        </div>
-        <div className="flex items-center w-full space-x-2 max-w-[360px]">
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-          <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
-          <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-        </div>
+const Skeleton: React.FC<SkeletonProps> = ({ sm }) => {
+  {
+    sm ? (
+      <div role="status" className="max-w-sm animate-pulse">
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
         <span className="sr-only">Loading...</span>
       </div>
-    </Card>
-  );
+    ) : (
+      <Card customClasses="p-4 m-4">
+        <div role="status" className="space-y-2.5 animate-pulse max-w-lg">
+          <div className="flex items-center space-x-2 w-full">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <div className="flex items-center w-full space-x-2 max-w-[480px]">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          </div>
+          <div className="flex items-center w-full space-x-2 max-w-[400px]">
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <div className="flex items-center w-full space-x-2 max-w-[480px]">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+          </div>
+          <div className="flex items-center w-full space-x-2 max-w-[440px]">
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
+          </div>
+          <div className="flex items-center w-full space-x-2 max-w-[360px]">
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-80"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </Card>
+    );
+  }
 };
 
 export default Skeleton;
