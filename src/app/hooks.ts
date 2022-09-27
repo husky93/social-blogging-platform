878267c187321be86c55c415572b5527fa6e7b46
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-27 13:21:47 +0200

    Create custom hook for fetching Blog Homepage data

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,8 +1,25 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged, auth } from './firebase';
+import { useEffect, useState, useCallback, useRef } from 'react';
+import {
+  onAuthStateChanged,
+  auth,
+  getDocs,
+  query,
+  startAfter,
+  startAt,
+  collection,
+  db,
+  orderBy,
+  limit,
+} from './firebase';
 import { login, logout } from '../features/user/userSlice';
+import type {
+  DocumentData,
+  QueryDocumentSnapshot,
+  Query,
+  QuerySnapshot,
+} from 'firebase/firestore';
+import type { RootState, AppDispatch } from './store';
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
@@ -26,4 +43,47 @@ export const useCheckIfLoggedIn = (dispatch: AppDispatch) => {
     });
   }, []);
   return loading;
+};
+
+export const useInfiniteLoading = () => {
+  const [posts, setPosts] = useState<Array<DocumentData>>([]);
+  const initialPageLoaded = useRef(false);
+  const [hasMore, setHasMore] = useState(true);
+  const [lastVisible, setLastVisible] =
+    useState<QueryDocumentSnapshot<DocumentData> | null>(null);
+
+  const loadItems = async () => {
+    const q = initialPageLoaded.current
+      ? query(
+          collection(db, 'posts'),
+          orderBy('likesCount', 'desc'),
+          limit(4),
+          startAfter(lastVisible)
+        )
+      : query(collection(db, 'posts'), orderBy('likesCount', 'desc'), limit(4));
+    const documentSnapshots: QuerySnapshot<DocumentData> = await getDocs(q);
+    await setPosts((prevState) => {
+      const dataArray = documentSnapshots.docs.map((item) => {
+        return { ...item.data(), id: item.id };
+      });
+      return [...prevState, ...dataArray];
+    });
+    setLastVisible(documentSnapshots.docs[documentSnapshots.docs.length - 1]);
+    await setHasMore(documentSnapshots.docs.length < 4 ? false : true);
+  };
+
+  useEffect(() => {
+    if (initialPageLoaded.current) {
+      return;
+    }
+
+    loadItems();
+    initialPageLoaded.current = true;
+  }, [loadItems]);
+
+  return {
+    posts,
+    hasMore,
+    loadItems,
+  };
 };
