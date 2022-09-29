39caf130710c69ecbe4d659a38b4711404bf489d
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-29 09:05:46 +0200

    Modify fetchPost method to check if post was fetched before querying db - Checks if post with the same id already exists in an array provided as a parameter.

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,5 +1,6 @@
-import React from 'react';
 import { db, doc, getDoc } from './firebase';
+import { addPost } from '../features/posts/postsSlice';
+import type { AppDispatch } from './store';
 import type {
   DocumentReference,
   DocumentData,
@@ -7,14 +8,27 @@ import type {
 } from 'firebase/firestore';
 
 export const fetchPost = async (
-  postID: string,
-  setPosts: (value: React.SetStateAction<DocumentData[]>) => void
-): Promise<void> => {
-  const docRef: DocumentReference<DocumentData> = doc(db, 'posts', postID);
-  const docSnap: DocumentSnapshot<DocumentData> = await getDoc(docRef);
-  const data: DocumentData | undefined = docSnap.data();
-  if (data) {
-    data.id = postID;
-    setPosts((prevState) => [...prevState, data]);
+  postID: string | undefined,
+  postsArray: Array<DocumentData>,
+  dispatch: AppDispatch
+): Promise<DocumentData | undefined> => {
+  if (!postID) return;
+
+  const desiredPost: DocumentData | undefined = postsArray.find(
+    (post) => post.id === postID
+  );
+
+  if (desiredPost === undefined) {
+    console.log('yo');
+    const docRef: DocumentReference<DocumentData> = doc(db, 'posts', postID);
+    const docSnap: DocumentSnapshot<DocumentData> = await getDoc(docRef);
+    const data: DocumentData | undefined = docSnap.data();
+    if (data) {
+      data.id = postID;
+      dispatch(addPost(data));
+      return data;
+    }
+  } else {
+    return desiredPost;
   }
 };
