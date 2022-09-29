f9c7d44b910bc0820cbe4abb43cddeea2b6d7d27
Maciej Sroka <sroka.maciej93@gmail.com>
2022-09-29 08:52:08 +0200

    Add posts reducer to the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice';
 import alertReducer from '../features/alert/alertSlice';
+import postsReducer from '../features/posts/postsSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     alert: alertReducer,
+    posts: postsReducer,
   },
 });
 
