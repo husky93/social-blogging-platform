69380d7de42b1a0e4a3f770a2c386ec4d0cbc028
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-01 14:01:14 +0200

    Updates

--- assets/Card.2a8d0762.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index.3ddb063d.js";const o=({children:r,customClasses:a})=>{const d=`w-full bg-white rounded-lg border shadow-md dark:bg-gray-800 dark:border-gray-700 ${a}`;return s("div",{className:d,children:r})};export{o as default};
