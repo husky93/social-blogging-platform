6ab21b2ccb088e2ae8e8e7cb7c33b4d3d09b8985
Maciej Sroka <sroka.maciej93@gmail.com>
2022-10-22 12:59:30 +0200

    Updates

---
+++ assets/Badge.d2c5a6ec.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.803f8f97.js";const a=({children:e})=>t("span",{className:"bg-gray-100 transition-all text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 hover:bg-gray-200",children:e});export{a as default};
