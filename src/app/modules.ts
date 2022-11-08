deb0c8c1284eea495e9110827a3201eeeaba9296
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-08 10:25:27 +0100

    Create function that caluclates time passed between two dates

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -1,5 +1,6 @@
 import { db, doc, getDoc } from './firebase';
 import { addPost } from '../features/posts/postsSlice';
+import { intervalToDuration } from 'date-fns';
 import type { AppDispatch } from './store';
 import type {
   DocumentReference,
@@ -30,4 +31,29 @@ export const fetchPost = async (
   } else {
     return desiredPost;
   }
+};
+
+export const getTimeBetween = (start: Date): string => {
+  const timeObj = intervalToDuration({
+    start,
+    end: new Date(),
+  });
+  console.log(timeObj);
+  if (timeObj.years && timeObj.years > 0)
+    return `${timeObj.years} ${timeObj.years > 1 ? 'years' : 'year'} ago`;
+  if (timeObj.months && timeObj.months > 0)
+    return `${timeObj.months} ${timeObj.months > 1 ? 'months' : 'month'} ago`;
+  if (timeObj.days && timeObj.days > 0)
+    return `${timeObj.days} ${timeObj.days > 1 ? 'days' : 'day'} ago`;
+  if (timeObj.hours && timeObj.hours > 0)
+    return `${timeObj.hours} ${timeObj.hours > 1 ? 'hours' : 'hour'} ago`;
+  if (timeObj.minutes && timeObj.minutes > 0)
+    return `${timeObj.minutes} ${
+      timeObj.minutes > 1 ? 'minutes' : 'minute'
+    } ago`;
+  if (timeObj.seconds && timeObj.seconds > 0)
+    return `${timeObj.seconds} ${
+      timeObj.seconds > 1 ? 'seconds' : 'second'
+    } ago`;
+  return 'unknown time ago';
 };
